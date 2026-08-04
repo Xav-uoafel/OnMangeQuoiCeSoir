aef331fbb5d44ce4e1bc9b49fd6799672fb59254
Xavier Le Faou <lefaoux@hotmail.fr>
2026-08-04 11:46:55 +0200

    Improve expired password reset recovery

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,14 @@
 module Users
   class PasswordsController < Devise::PasswordsController
+    def edit
+      super
+
+      token_owner = resource_class.with_reset_password_token(params[:reset_password_token])
+      return if token_owner&.reset_password_period_valid?
+
+      resource.errors.add(:reset_password_token, token_owner ? :expired : :invalid)
+    end
+
     protected
 
     def after_sending_reset_password_instructions_path_for(resource_name)
--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -55,10 +55,62 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
     assert_equal I18n.t("devise.passwords.send_paranoid_instructions"), flash[:notice]
   end
 
+  test "expired reset token offers a new link when the page opens" do
+    user = users(:one)
+    reset_token = expired_reset_token_for(user)
+
+    get edit_user_password_path(reset_password_token: reset_token)
+
+    assert_response :success
+    assert_select "h1", text: "Ce lien a expiré"
+    assert_select "a[href='#{new_user_password_path}']", text: "Recevoir un nouveau lien"
+    assert_select "input[type='password']", count: 0
+    assert_no_match(/Translation missing/, response.body)
+  end
+
+  test "expired reset token cannot change the password" do
+    user = users(:one)
+    reset_token = expired_reset_token_for(user)
+
+    patch user_password_path, params: {
+      user: {
+        reset_password_token: reset_token,
+        password: "new-password123",
+        password_confirmation: "new-password123"
+      }
+    }
+
+    assert_response :unprocessable_entity
+    assert_select "h1", text: "Ce lien a expiré"
+    assert_select "a[href='#{new_user_password_path}']", text: "Recevoir un nouveau lien"
+    assert_select "input[type='password']", count: 0
+    assert_no_match(/Translation missing/, response.body)
+    assert_not user.reload.valid_password?("new-password123")
+  end
+
+  test "invalid reset token offers a new link" do
+    get edit_user_password_path(reset_password_token: "invalid-token")
+
+    assert_response :success
+    assert_select "h1", text: "Ce lien n’est plus valide"
+    assert_select "a[href='#{new_user_password_path}']", text: "Recevoir un nouveau lien"
+    assert_select "input[type='password']", count: 0
+  end
+
   private
 
   def reset_link_from(email)
     document = Nokogiri::HTML(email.html_part.body.decoded)
     URI.parse(document.at_css('a[href*="reset_password_token"]')["href"])
+  end
+
+  def expired_reset_token_for(user)
+    assert_emails 1 do
+      post user_password_path, params: { user: { email: user.email } }
+    end
+
+    reset_url = reset_link_from(ActionMailer::Base.deliveries.last)
+    user.update!(reset_password_sent_at: 7.hours.ago)
+    Rack::Utils.parse_query(reset_url.query).fetch("reset_password_token")
   end
 end
